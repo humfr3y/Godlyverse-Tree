d662e532f805cb25703f7c7d705eb1da1cebbdbe
Humfrey <madkotodax@gmail.com>
2022-10-28 23:33:08 +0300

    update layers js

--- js/layers.js
+++ js/layers.js
@@ -1,15 +1,15 @@
-addLayer("p", {
+addLayer("n", {
     name: "NPS Shop", // This is optional, only used in a few places, If absent it just uses the layer id.
     symbol: "NPS", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
-		points: new Decimal(0),
+		points: new Decimal(1),
     }},
     color: "#1AA7EC",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
     resource: "NPS", // Name of prestige currency
-    baseResource: "NPS", // Name of resource prestige is based on
+    baseResource: "Number", // Name of resource prestige is based on
     baseAmount() {return player.points}, // Get the current amount of baseResource
     type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
     exponent: 0.5, // Prestige currency exponent
@@ -21,6 +21,34 @@ addLayer("p", {
         return new Decimal(1)
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
+    hotkeys: [
+        {key: "n", description: "N: Reset for number per second", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+    ],
+    layerShown(){return true}
+}),
+addLayer("p", {
+    name: "Prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: false,
+		points: new Decimal(0),
+    }},
+    color: "#11FF22",
+    requires: new Decimal(4), // Can be a function that takes requirement increases into account
+    resource: "Prestige Points", // Name of prestige currency
+    baseResource: "Number", // Name of resource prestige is based on
+    baseAmount() {return player.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0.5, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 1, // Row the layer is in on the tree (0 is the first row)
     hotkeys: [
         {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
